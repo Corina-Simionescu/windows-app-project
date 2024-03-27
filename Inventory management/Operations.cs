96e3da2efd07bf4fa2b2178b01e6281b028a04f4
Corina717 <corina.simionescu12@gmail.com>
2024-03-27 19:59:41 +0200

    add Operation class + test in main

---
+++ Inventory management/Operations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_management
+{
+    internal class Operations
+    {
+        private List<Item> items;
+
+        public Operations()
+        {
+            items = new List<Item>();
+        }
+
+        public Operations(List<Item> items) 
+        {
+            this.items = items;
+        }
+
+        public void addItem(Item item)
+        {
+            items.Add(item);
+        }
+
+        public void removeItem(Item item)
+        {
+            items.Remove(item);
+        }
+
+        public void printItems()
+        {
+            foreach(Item item in items)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
--- Inventory management/Program.cs
+++ Inventory management/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,22 @@ namespace Inventory_management
     {
         static void Main(string[] args)
         {
-            Item i1 = new Item();
-            Item i2 = new Item(1, "a", "b", 1, 10);
+            Item item1 = new Item();
+            Item item2 = new Item(2, "item2", "bbb", 2, 20);
+            Item item3 = new Item(3, "item3", "asdd", 300, 110);
+            Item item4 = new Item(4, "item4", "aaaaa", 1000, 1);
+
+            List<Item> items = new List<Item>();
+
+            items.Add(item1);
+            items.Add(item2);
+            items.Add(item3);
+
+            Operations operation1 = new Operations(items);
+
+            operation1.addItem(item4);
+            operation1.removeItem(item1);
+            operation1.printItems();
         }
     }
 }
